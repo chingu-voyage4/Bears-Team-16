03e6fa3e939f7cec6a5afe9b6349531c50727fbb
osycon <fredrichawk@gmail.com>
2018-02-24 15:35:09 +0100

    Added airbnb eslint setup.

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const express = require('express');
-const morgan = require('morgan');
-const bodyParser = require('body-parser');
+const express = require(`express`);
+const morgan = require(`morgan`);
+const bodyParser = require(`body-parser`);
 
 const app = express();
 const PORT = process.env.PORT || 3005;
@@ -8,11 +8,11 @@ const PORT = process.env.PORT || 3005;
 // Application config
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.use(morgan('dev'));
+app.use(morgan(`dev`));
 
 // Route
-app.get('/', (req, res) => {
-  res.json('Howdy');
+app.get(`/`, (req, res) => {
+  res.json(`Howdy`);
 });
 
 app.listen(PORT, console.log(`There will be dragons on port ${PORT}.`));
