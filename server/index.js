ea0032075fe7fe7c47604adb85372c22f476199e
thinktwice13 <marioo.krajacic@gmail.com>
2018-02-24 03:23:06 +0100

    Setup simple express server

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const morgan = require("morgan");
+const bodyParser = require("body-parser");
+
+const app = express();
+const PORT = process.env.PORT || 3005;
+
+// Application config
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(morgan("dev"));
+
+// Route
+app.get("/", (req, res) => {
+  res.json("Howdy");
+});
+
+app.listen(PORT, console.log(`There will be dragons on port ${PORT}.`));
