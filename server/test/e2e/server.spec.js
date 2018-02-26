85836df5226b3611d6546e3730fc552991095bd4
thinktwice13 <marioo.krajacic@gmail.com>
2018-02-26 23:13:22 +0100

    Fix ESlint config Update files according to new rules

--- server/test/e2e/server.spec.js
+++ server/test/e2e/server.spec.js
@@ -3,16 +3,16 @@ import request from 'supertest';
 import { app } from '../../src';
 // Example test
 describe(`GET /`, () => {
-  it(`should return status 200`, () =>
-    request(app)
-      .get(`/`)
-      .send()
-      .expect(200));
-  it(`should return 'Howdy'`, () =>
-    request(app)
-      .get(`/`)
-      .send()
-      .then(res => {
-        expect(res.body).to.equal(`Howdy`);
-      }));
+    it(`should return status 200`, () =>
+        request(app)
+            .get(`/`)
+            .send()
+            .expect(200));
+    it(`should return 'Howdy'`, () =>
+        request(app)
+            .get(`/`)
+            .send()
+            .then(res => {
+                expect(res.body).to.equal(`Howdy`);
+            }));
 });
