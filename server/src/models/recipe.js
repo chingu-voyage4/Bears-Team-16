5ec4b501c361afcb80d26014b39c8e8104d89e81
thinktwice13 <marioo.krajacic@gmail.com>
2018-03-02 01:20:29 +0100

    Setup DB bookshelf models

---
+++ server/src/models/recipe.js
@@ -0,0 +1,12 @@
+import db from '../db';
+import User from "../models/user";
+
+const Recipe = db.Model.extend({
+    tableName: `recipes`,
+    hasTimestamps: true,
+    author() {
+        return this.belongTo(User);
+    },
+});
+
+export default Recipe;
---
+++ server/src/models/user.js
@@ -0,0 +1,15 @@
+import db from "../db";
+import Recipe from "../models/recipe";
+
+const User = db.Model.extend({
+    tableName: `users`,
+    hasTimestamps: true,
+    recipes() {
+        return this.hasMany(Recipe);
+    },
+    favs() {
+        return this.belongsToMany(Recipe, `favs`);
+    },
+});
+
+export default User;
