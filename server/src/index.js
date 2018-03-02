56d7ba1f90b6df74efe2d781a2a12fe9c2fd84bd
thinktwice13 <marioo.krajacic@gmail.com>
2018-03-02 01:27:32 +0100

    Update express server with graphql endpoint

--- server/src/index.js
+++ server/src/index.js
@@ -2,9 +2,11 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
+import graphqlHTTP from "express-graphql";
+import schema from './graphql/';
 
 // Application config
-dotenv.config({ path: `${__dirname  }/../../.env` });
+dotenv.config({ path: `${__dirname}/../../.env` });
 const { env } = process;
 export const app = express();
 
@@ -14,8 +16,17 @@ app.use(morgan(`dev`));
 app.use(require(`cors`)());
 
 // Route
-app.get(`/`, (req, res) => {
-    res.json(`Howdy`);
-});
+app.get(`/`, (req, res) => { res.json(`Howdy`); });
+
+// GraphiQL
+app.use(
+    `/graphql`,
+    bodyParser.json(),
+    graphqlHTTP({
+        schema,
+        graphiql: true,
+    }),
+);
+
 
 app.listen(env.PORT, console.log(`There will be recipes on ${env.HOST}:${env.PORT}.`));
