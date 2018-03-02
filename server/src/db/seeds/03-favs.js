7e23f31b8c56c690aa263749526b47fb946e57ed
thinktwice13 <marioo.krajacic@gmail.com>
2018-03-02 01:19:10 +0100

    Create DB seeds

---
+++ server/src/db/seeds/03-favs.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+
+export const seed = (knex, Promise) =>
+    // Deletes ALL existing entries
+    knex(`favs`)
+        .del()
+        .then(() =>
+            // Inserts seed entries
+            knex(`favs`).insert([
+                {
+                    recipe_id: 3,
+                    user_id: 1,
+                },
+                {
+                    recipe_id: 2,
+                    user_id: 1,
+                },
+                {
+                    recipe_id: 2,
+                    user_id: 3,
+                },
+            ]));
