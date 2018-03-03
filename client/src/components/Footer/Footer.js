e3558be42c96788bb8bba63d02f1e9387e675e29
Ayumi <ayumisaito27@gmail.com>
2018-03-03 17:14:05 +0100

    first commit. add Header and Footer components

---
+++ client/src/components/Footer/Footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => (
+    <Footer>Footer</Footer>
+)
+
+export default Footer;
---
+++ client/src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => (
+<Header>
+    
+</Header>
+        )
+
+export default Header;
