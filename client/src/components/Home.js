e4f76b7fda21a1e3ffaf56b9c2bc07f64f0bb30d
thinktwice13 <marioo.krajacic@gmail.com>
2018-03-06 12:32:14 +0100

    Integrate GraphQL with React Add sample query Closes #38

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { gql } from "apollo-boost";
+import { graphql } from "react-apollo";
 
-const Home = () => (
-    <div>
-        <h1>Recipe App</h1>
-    </div>
+const fetchRecipes = gql`
+query {
+  recipes {
+    id
+    author {
+      uname
+    }
+    title
+  }
+}
+`;
+
+const Home = ({ data, loading, error }) => (
+  <div>
+    <h1>Recipe app</h1>
+    <ul>{!data.loading && data.recipes.map(recipe =>
+      <li key={recipe.id}>{`${recipe.title} by ${recipe.author.uname}`}</li>)}
+    </ul>
+  </div>
 );
 
-export default Home;
+export default graphql(fetchRecipes)(Home);
--- client/src/index.js
+++ client/src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
 import './index.css';
-import './assets/style.css'
+import './assets/style.css';
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById(`root`));
+const client = new ApolloClient({
+  uri: `http://localhost:4000/graphql`,
+});
+
+ReactDOM.hydrate(
+  <ApolloProvider client={client}>
+    <App />
+  </ApolloProvider>,
+  document.getElementById(`root`),
+);
 registerServiceWorker();
