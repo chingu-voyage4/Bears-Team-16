e88e901aa5a207ba0c946090b586265eef8a4a05
thinktwice13 <marioo.krajacic@gmail.com>
2018-03-07 18:41:10 +0100

    Setup integration tests environment Closes #41

--- server/src/db/dbConfig.js
+++ server/src/db/dbConfig.js
@@ -13,5 +13,10 @@ module.exports = {
     min: 2,
     max: 10,
   },
+  migrations: {
+    directory: `${__dirname}/migrations`,
+    tableName: `knex_migrations`,
+  },
+  seeds: { directory: `${__dirname}/seeds` },
   debug: process.env.NODE_ENV === `development`,
 };
