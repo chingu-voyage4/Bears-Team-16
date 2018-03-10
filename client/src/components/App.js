562310488bd641bd35129d86ba1aee17cc1ca5b1
Ayumi <ayumisaito27@gmail.com>
2018-03-10 11:46:11 +0100

    add React Router test

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,7 +13,7 @@ import Home from "./Home";
 
 const App = () => (
     <Router>
-        <div className='test'>
+        <div>
             <Header />
             <Route exact path="/" component={Home} />
             <Route path="/login" component={Login} />
--- client/src/components/App.test.js
+++ client/src/components/App.test.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import enzyme, { shallow } from 'enzyme';
-import App from './App';
+import enzyme, { mount } from 'enzyme';
 import Adapter from "enzyme-adapter-react-16";
+import { MemoryRouter } from 'react-router-dom';
+import App from './App';
+import { Home } from "./Home";
 
 enzyme.configure({ adapter: new Adapter() });
 
-
-it(`sample test`, () => {
-  expect(2 + 2).toBe(4);
-});
-
-it(`sample test for enzyme`, () => {
-  shallow(<App />);
-});
+it('renders correct routes', () => {
+    const wrapper = mount(<MemoryRouter initialEntries={['/']} initialIndex={0}><App /></MemoryRouter>);
+    expect(wrapper.contains({ Home })).toBe(true);
+})
