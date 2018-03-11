1ceab3b5f2240b0b43a385f2683a868b3b066a06
Ayumi <ayumisaito27@gmail.com>
2018-03-11 17:43:30 +0100

    add error

--- client/src/components/auth/Signup/Signup.js
+++ client/src/components/auth/Signup/Signup.js
@@ -2,14 +2,41 @@ import React from 'react';
 import './Signup.css'
 
 class Signup extends React.Component{
+constructor(props){
+    super(props)
+    this.state={
+        error : true
+    }
+}
+    onChange=(e)=> {
+     let val = e.target.value
+       if(val.length > 0){
+         this.setState({ error : false})
+       }
+    }
+
+    onSubmit=(e)=> {
+    e.preventDefault();
+    if(this.state.error){
+       alert('error')
+    }
+    }
+
     render(){
         return(
             <div className='sign-up-wrapper'>
-                <div className='sign-up-inner'>
-                <input type='text' placeholder='Email'/>
-                <input type='password' placeholder='Password' />
+                    <form className='sign-up-inner' onSubmit={(e)=>this.onSubmit(e)}>
+                <input type='text'
+                       placeholder='Email'
+                       onChange={(e)=>this.onChange(e)}
+                />
+                <input type='password'
+                       placeholder='Password'
+                       onChange={(e)=>this.onChange(e)}
+                />
+                        <button>Submit</button>
+                    </form>
                 </div>
-            </div>
         )
     }
 }
