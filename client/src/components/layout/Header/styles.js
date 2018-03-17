cf22dfe42378449d5bd61adbfd280c3ae2529e75
Ayumi <ayumisaito27@gmail.com>
2018-03-17 17:49:34 +0100

    fix router

--- client/src/components/layout/Header/styles.js
+++ client/src/components/layout/Header/styles.js
@@ -1,4 +1,5 @@
 import glamorous from 'glamorous';
+import Browse from './img/Browse.png'
 
 export const HeaderStyle = glamorous.div({
     backgroundColor: '#C8E6C9',
@@ -20,7 +21,46 @@ export const NavBarStyle = glamorous.div({
 });
 
 export const NavStyle = glamorous.div({
-    marginRight : 10,
+    margin: '20px 10px',
     textDecoration: 'none',
+    color: 'black',
+    display: 'flex',
+    justifyContent: 'space-between'
+})
+
+export const NavItem = glamorous.p({
+    margin: '0',
     color: 'black'
+})
+
+export const Input = glamorous.input({
+    height: 60,
+    width: 329,
+    backgroundColor: '#A0DBA2',
+    border: 'none',
+    margin: '20px 20px'
+})
+
+export const SignUpButton = glamorous.button({
+    height: 55,
+    width: 115,
+    backgroundColor: '#E5E5E5',
+    fontFamily: 'Nunito',
+    color: 'white',
+    fontWeight: 900,
+    border: 'none',
+    borderRadius: 4,
+    padding: '10px 20px',
+    margin: '20px 20px',
+    cursor: 'pointer',
+    boxShadow: '0 4px 6px rgba(50,50,93,.11), 0 1px 3px rgba(0,0,0,.08)',
+    ':hover': {
+        backgroundColor: '#E6E6E6',
+        transform: 'translateY(-1px)',
+        boxShadow: '0 7px 14px rgba(50,50,93,.1), 0 3px 6px rgba(0,0,0,.08)'
+    },
+    ':focus': { outline: 0},
+    ':active' : {
+        transform: 'translateY(1px)'
+    }
 })
