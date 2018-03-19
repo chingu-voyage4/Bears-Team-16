7d7f0ab764574e4304559583d1307a03290ba8c0
thinktwice13 <marioo.krajacic@gmail.com>
2018-03-19 15:35:37 +0100

    Add db seed generator tests Add db seed templates tests (sample)

---
+++ server/src/utils/helpers.js
@@ -0,0 +1,16 @@
+// Helpers
+// Flatten seed tmeplate values
+// TODO replace with .reduce
+export const flattenTemplates = (templates, test = []) => {
+  Object.values(templates).forEach(template => {
+    Object.values(template).forEach(args => {
+      test.push(args);
+    });
+  });
+  return test;
+};
+
+// Ivert 2D array to test for types
+export const invert2D = arr =>
+  arr[0].map((col, i) =>
+    arr.map(row => row[i]));
