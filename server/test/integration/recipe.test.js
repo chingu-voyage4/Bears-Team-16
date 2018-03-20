c608b4de31ba852f534a9f1f03106a8256eaf52a
thinktwice13 <marioo.krajacic@gmail.com>
2018-03-20 01:04:49 +0100

    Update query:recipes test

--- server/test/integration/recipe.test.js
+++ server/test/integration/recipe.test.js
@@ -3,27 +3,40 @@ import chai from "chai";
 import { reseed } from "../helpers";
 import schema from "../../src/graphql";
 
+chai.should();
+chai.use(require(`chai-things`));
+
 require(`../../src`);
 
 chai.use(require(`chai-http`));
 const { expect } = chai;
-const Query = schema.getType(`Query`).getFields();
+const recipes = schema.getType(`Query`).getFields().recipes.resolve;
 
-// describe(`Recipe`, () => {
-//   describe(`query:recipe`, () => {
-//     it(`return an object with property 'id'`, () => {
-//       expect(Query.recipe.resolve()).to.have.property(`id`);
-//       expect(Query.recipe.resolve()).to.deep.equal({ id: `I'm a recipe!` });
-//     });
-//   });
+describe(`Recipe`, () => {
+  describe(`query:recipes`, () => {
+    it(`without params, returns an array of recipes with correct fields`, () =>
+      recipes(null, {}).then(data => {
+        data.should.not.be.an(`string`);
+        data.should.be.an(`array`);
+        [
+          `id`,
+          `title`,
+          `description`,
+          `author`,
+          `prep_time`,
+        ].forEach(prop => {
+          data.should.all.have.property(prop);
+        });
+      }));
+  });
 
-//   describe(`query:recipes`, () => {
-//     // reseed before each test
-//     reseed();
+  // describe(`query:recipes`, () => {
+  //   // reseed before each test
+  //   reseed();
 
-//     it(`returns an array`, () =>
-//       Query.recipes.resolve().then(data => {
-//         expect(data).to.be.an(`array`);
-//       }));
-//   });
-// });
+  //   it(`returns an array`, () =>
+  //     Query.recipes.resolve().then(data => {
+  //       expect(data).to.be.an(`array`);
+  //     }));
+  // });
+});
