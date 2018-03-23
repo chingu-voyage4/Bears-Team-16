c1e493c7bdc3f8849d7dae68c9d1c71079610f9a
thinktwice13 <marioo.krajacic@gmail.com>
2018-03-23 10:34:01 +0100

    Temporary fix for database resed issue: Removed unique contraint on db users.fname and users.lname CLoses #78

--- server/src/db/migrations/20180228214456_users.js
+++ server/src/db/migrations/20180228214456_users.js
@@ -1,26 +1,22 @@
-export const up = (knex, Promise) => Promise.all([
-  knex.schema.createTable(`users`, table => {
-    table.increments(`id`).primary();
-    table
-      .string(`email`)
-      .notNullable()
-      .unique();
-    table.string(`password`).notNullable();
-    table
-      .string(`fname`)
-      .unique();
-    table
-      .string(`lname`)
-      .unique();
-    table
-      .enu(`unit_system`, [ `metric`, `imperial` ])
-      .defaultTo(`metric`);
-    table.integer(`portions`).defaultTo(4);
-    table.string(`avatar`);
-    table.text(`bio`);
-    table.string(`location`);
-    table.timestamps();
-  }),
-]);
+export const up = (knex, Promise) =>
+  Promise.all([
+    knex.schema.createTable(`users`, table => {
+      table.increments(`id`).primary();
+      table
+        .string(`email`)
+        .notNullable()
+        .unique();
+      table.string(`password`).notNullable();
+      table.string(`fname`);
+      table.string(`lname`);
+      table.enu(`unit_system`, [ `metric`, `imperial` ]).defaultTo(`metric`);
+      table.integer(`portions`).defaultTo(4);
+      table.string(`avatar`);
+      table.text(`bio`);
+      table.string(`location`);
+      table.timestamps();
+    }),
+  ]);
 
-export const down = (knex, Promise) => Promise.all([ knex.schema.dropTable(`users`) ]);
+export const down = (knex, Promise) =>
+  Promise.all([ knex.schema.dropTable(`users`) ]);
