f75253e09c6b0ccea9c1e2b64763e52b27170fe4
Ayumi <ayumisaito27@gmail.com>
2018-03-28 08:09:49 +0200

    replace png image with svg image header/footer components

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,10 +1,44 @@
 import React from 'react';
+import { NavBarStyle, NavStyle} from '../Header/styles.js'
+import { FooterStyle,FooterLink,FooterText,FooterIconStyle,FooterSubText } from './styles.js'
+import Facebook from './img/Facebook.svg'
+import Gplus from './img/Gplus.svg'
+import Instagram from './img/Instagram.svg'
+import Pinterest from './img/Pinterest.svg'
+import Twitter from './img/Twitter.svg'
+
 import './Footer.css'
 
 const Footer = () => (
     <div>
-        <h1>Footer</h1>
+    <FooterStyle>
+        <FooterIconStyle css={{ gridArea: 'footer-icon'}}  style={{ textAlign: 'center'}}>
+            <NavStyle><img style={{ height: '50px'}} src={Facebook}/></NavStyle>
+            <NavStyle><img style={{ height: '50px'}} src={Gplus}/></NavStyle>
+            <NavStyle><img style={{ height: '50px'}} src={Instagram}/></NavStyle>
+            <NavStyle><img style={{ height: '50px'}} src={Pinterest}/></NavStyle>
+            <NavStyle><img style={{ height: '50px'}} src={Twitter}/></NavStyle>
+        </FooterIconStyle>
+
+
+            <FooterText css={{ gridArea: 'footer-text'}}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Etiam placerat erat pharetra facilisis ultricies.
+                Pellentesque ut consectetur mauris. Proin sit.
+                Pellentesque ut consectetur mauris. Proin sit.</FooterText>
+
+        <FooterSubText>Find us all over Internet.
+       Seriously. Everywhere. </FooterSubText>
+
+
+    </FooterStyle>
+    <FooterLink>
+        <div style={{ textAlign: 'center'}}>
+            © Bear recipes 2018  </div>
+        <div style={{ textAlign: 'center'}}>  Built by Bears Team 16 for Chingu Voyage 4 </div>
+    </FooterLink>
     </div>
 );
 
 export default Footer;
+
--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -2,31 +2,33 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Header.css'
 import {HeaderStyle, NavBarStyle, NavStyle, Input, NavItem, SignUpButton} from './styles.js'
-import About from './img/About.png'
-import Home from './img/Home.png'
-import Browse from './img/Browse.png'
-import Contact from './img/Contact.png'
+import About from './img/About.svg'
+import Home from './img/Home.svg'
+import Browse from './img/BrowseRecipes.svg'
+import Contact from './img/Contact.svg'
 
 const Header = () => (
+    <div>
     <HeaderStyle>
         <div css={{ gridArea: 'header-logo'}}>logo</div>
         <NavBarStyle css={{ gridArea: 'header-navbar'}}>
             <NavStyle>
-                <Link to="/login"><img src={Home}/><NavItem>HOME</NavItem></Link>
+                <Link to="/login" style={{ textDecoration: 'none'}}><img src={Home}/><NavItem>HOME</NavItem></Link>
             </NavStyle>
             <NavStyle>
-                <Link to="/browse"><img src={Browse}/><NavItem>BROWSE</NavItem></Link>
+                <Link to="/browse" style={{ textDecoration: 'none'}}><img src={Browse}/><NavItem>BROWSE</NavItem></Link>
             </NavStyle>
             <NavStyle>
-                <Link to="/about"><img src={About}/><NavItem>ABOUT</NavItem></Link>
+                <Link to="/about" style={{ textDecoration: 'none'}}><img src={About}/><NavItem>ABOUT</NavItem></Link>
             </NavStyle>
             <NavStyle>
-                <Link to="/contact"><img src={Contact}/><NavItem>CONTACT</NavItem></Link>
+                <Link to="/contact" style={{ textDecoration: 'none'}}><img src={Contact}/><NavItem>CONTACT</NavItem></Link>
             </NavStyle>
             <Input placeholder='Search for recipes..' />
             <SignUpButton>SIGN IN</SignUpButton>
         </NavBarStyle>
     </HeaderStyle>
+    </div>
 );
 
 export default Header;
