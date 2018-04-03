49114e04714482c39493bd7c6b647e84616ebc4c
osycon <fredrichawk@gmail.com>
2018-04-03 11:03:35 +0200

    Added classname to form.

--- client/src/components/recipes/AddRecipe.js
+++ client/src/components/recipes/AddRecipe.js
@@ -4,7 +4,9 @@ import { Form, Text, Checkbox, NestedField, Select } from "react-form";
 class AddRecipe extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      recipe: null,
+    };
   }
   render() {
     console.log(this.state.recipe);
@@ -28,61 +30,59 @@ class AddRecipe extends Component {
     return (
       <div>
         <h1>Add Recipe</h1>
-        <h3>General Recipe Options</h3>
         <Form onSubmit={recipe => this.setState({ recipe })}>
           {formApi => (
             <div>
-              <form onSubmit={formApi.submitForm} id="dynamic-form">
-                <label htmlFor="title">Recipe Name</label>
-                <Text
-                  id="title"
-                  field="title"
-                  placeholder="ex. Jambalaya"
-                  type="text"
-                />
-                <br />
-                <label htmlFor="portions">Portions</label>
-                <Text
-                  id="portions"
-                  field="portions"
-                  placeholder="ex. 2"
-                  type="number"
-                  min="0"
-                  max="12"
-                  step="1"
-                />
-                <br />
-                <br />
-                <label htmlFor="cooktime">Cooktime in minutes</label>
-                <Text
-                  id="cooktime"
-                  field="cooktime"
-                  placeholder="ex. 45"
-                  type="number"
-                  min="0"
-                  max="300"
-                  step="1"
-                />
-                <br />
-                <br />
-                <h3>Categories</h3>
-                <NestedField field="categories">
-                  <label htmlFor="beef">beef</label>
-                  <Checkbox field="beef" id="beef" style={style} />
-                  <br />
-                  <label htmlFor="fish">fish</label>
-                  <Checkbox field="fish" id="fish" style={style} />
-                  <br />
-                  <label htmlFor="pork">pork</label>
-                  <Checkbox field="pork" id="pork" style={style} />
-                  <br />
-                  <label htmlFor="pasta">pasta</label>
-                  <Checkbox field="pasta" id="pasta" style={style} />
-                </NestedField>
-                <br />
-                <br />
-                <h3>Ingredients</h3>
-                {formApi.values.ingredients &&
+              <form onSubmit={formApi.submitForm} id="addrecipe-form" className="recipe-form">
+                <div className="form__recipe-item">
+                  <h3>General Recipe Options</h3>
+                  <label htmlFor="title">Recipe Name</label>
+                  <Text
+                    id="title"
+                    field="title"
+                    placeholder="ex. Jambalaya"
+                    type="text"
+                  />
+                  <label htmlFor="portions">Portions</label>
+                  <Text
+                    id="portions"
+                    field="portions"
+                    placeholder="ex. 2"
+                    type="number"
+                    min="0"
+                    max="12"
+                    step="1"
+                  />
+                  <label htmlFor="cooktime">Cooktime in minutes</label>
+                  <Text
+                    id="cooktime"
+                    field="cooktime"
+                    placeholder="ex. 45"
+                    type="number"
+                    min="0"
+                    max="300"
+                    step="1"
+                  />
+                </div>
+                <div className="form__recipe-item">
+                  <h3>Categories</h3>
+                  <NestedField field="categories">
+                    <label htmlFor="beef">beef</label>
+                    <Checkbox field="beef" id="beef" style={style} />
+                    <br />
+                    <label htmlFor="fish">fish</label>
+                    <Checkbox field="fish" id="fish" style={style} />
+                    <br />
+                    <label htmlFor="pork">pork</label>
+                    <Checkbox field="pork" id="pork" style={style} />
+                    <br />
+                    <label htmlFor="pasta">pasta</label>
+                    <Checkbox field="pasta" id="pasta" style={style} />
+                  </NestedField>
+                </div>
+                <div className="form__recipe-item">
+                  <h3>Ingredients</h3>
+                  {formApi.values.ingredients &&
                   formApi.values.ingredients.map((ingredient, i) => (
                     <div key={`ingredient${i}`}>
                       <NestedField field={[ `ingredients`, i ]}>
@@ -121,17 +121,17 @@ class AddRecipe extends Component {
                       </NestedField>
                     </div>
                   ))}
-                <button
-                  onClick={() => formApi.addValue(`ingredients`, ``)}
-                  type="button"
-                  className="mb-4 mr-4 btn btn-success"
-                >
+                  <button
+                    onClick={() => formApi.addValue(`ingredients`, ``)}
+                    type="button"
+                    className="mb-4 mr-4 btn btn-success"
+                  >
                   Add Ingredient
-                </button>
-                <br />
-                <br />
-                <h3>Cooking steps</h3>
-                {formApi.values.steps &&
+                  </button>
+                </div>
+                <div className="form__recipe-item">
+                  <h3>Cooking steps</h3>
+                  {formApi.values.steps &&
                   formApi.values.steps.map((step, i) => (
                     <div key={`step${i}`}>
                       <label htmlFor={`step-name-${i}`}>
@@ -147,15 +147,14 @@ class AddRecipe extends Component {
                       </button>
                     </div>
                   ))}
-                <button
-                  onClick={() => formApi.addValue(`steps`, ``)}
-                  type="button"
-                  className="mb-4 mr-4 btn btn-success"
-                >
+                  <button
+                    onClick={() => formApi.addValue(`steps`, ``)}
+                    type="button"
+                    className="mb-4 mr-4 btn btn-success"
+                  >
                   Add Step
-                </button>
-                <br />
-                <br />
+                  </button>
+                </div>
                 <button type="submit" className="mb-4 btn btn-primary">
                   Submit
                 </button>
