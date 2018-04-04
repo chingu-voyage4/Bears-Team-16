f48870000a8e203eac8ccaa0e339a02a7525ded2
osycon <fredrichawk@gmail.com>
2018-04-04 07:58:58 +0200

    Fixed false positive tests.

--- client/src/components/recipes/AddRecipe.test.js
+++ client/src/components/recipes/AddRecipe.test.js
@@ -1,47 +1,73 @@
 import React from 'react';
-import enzyme, { shallow } from 'enzyme';
+import enzyme, { shallow, mount } from 'enzyme';
 import Adapter from "enzyme-adapter-react-16";
 import { Form, Text, Checkbox, NestedField, Select } from "react-form";
 
 import AddRecipe from './AddRecipe';
 
 enzyme.configure({ adapter: new Adapter() });
-// test(`Should be 2`, () => {
-//   expect(4 + 2).toBe(6);
-//   expect(2 - 2).toBe(0);
-//   expect(2 + 2).toBe(4);
-// });
 
 describe(`<AddRecipe />`, () => {
   it(`should have a heading with 'Add Recipe'`, () => {
     const wrapper = shallow(<AddRecipe />);
     expect(wrapper.contains(<h1>Add Recipe</h1>)).toEqual(true);
+    expect(wrapper.find(`h1`).length).toEqual(1);
   });
+
   it(`should have a From element`, () => {
     const wrapper = shallow(<AddRecipe />);
-    expect(wrapper.exists(<Form />)).toBe(true);
+    expect(wrapper.find(Form).length).toEqual(1);
   });
+
   it(`should have a form element`, () => {
     const wrapper = shallow(<AddRecipe />);
-    expect(wrapper.exists(<form className="recipe-form" />)).toBe(true);
+    expect(wrapper.find(Form).length).toEqual(1);
   });
+
   it(`should have a title input`, () => {
-    const wrapper = shallow(<AddRecipe />);
-    expect(wrapper.exists(<Text id="title" />)).toBe(true);
+    const wrapper = mount(<AddRecipe />);
+    expect(wrapper.find(`[htmlFor="title"]`).length).toEqual(1);
+    expect(wrapper.find(`input#title`).length).toEqual(1);
   });
+
   it(`should have a portions input`, () => {
-    const wrapper = shallow(<AddRecipe />);
-    expect(wrapper.exists(<Text id="portions" />)).toBe(true);
+    const wrapper = mount(<AddRecipe />);
+    expect(wrapper.find(`[htmlFor="portions"]`).length).toEqual(1);
+    expect(wrapper.find(`input#portions`).length).toEqual(1);
+  });
+
+  it(`should have a cooktime input`, () => {
+    const wrapper = mount(<AddRecipe />);
+    expect(wrapper.find(`[htmlFor="cooktime"]`).length).toEqual(1);
+    expect(wrapper.find(`input#cooktime`).length).toEqual(1);
+  });
+
+  it(`should have a NestedField with a class of recipe-categories`, () => {
+    const wrapper = mount(<AddRecipe />);
+    expect(wrapper.find(NestedField).length).toEqual(1);
+    expect(wrapper.find(`NestedField.recipe-categories`).length).toEqual(1);
+  });
+
+  it(`should have a NestedField with Checkboxes`, () => {
+    const wrapper = mount(<AddRecipe />);
+    expect(wrapper.find(Checkbox).length).toEqual(4);
+    expect(wrapper.find(`[htmlFor="beef"]`).length).toEqual(1);
+    expect(wrapper.find(`input#beef`).length).toEqual(1);
+    expect(wrapper.find(`[htmlFor="fish"]`).length).toEqual(1);
+    expect(wrapper.find(`input#fish`).length).toEqual(1);
+    expect(wrapper.find(`[htmlFor="pork"]`).length).toEqual(1);
+    expect(wrapper.find(`input#pork`).length).toEqual(1);
+    expect(wrapper.find(`[htmlFor="pasta"]`).length).toEqual(1);
+    expect(wrapper.find(`input#pasta`).length).toEqual(1);
+  });
+
+  it(`should have ingredients`, () => {
+    const wrapper = mount(<AddRecipe />);
+    expect(wrapper.find(`.ingredients`).length).toEqual(1);
+  });
+
+  it(`should have steps`, () => {
+    const wrapper = mount(<AddRecipe />);
+    expect(wrapper.find(`.steps`).length).toEqual(1);
   });
 });
-// it(`Fails`, () => {
-//   expect(1).toBe(0);
-// });
-
-// test(`should behave...`, () => {
-//   expect();
-// });
-
-// it(`should behave...`, () => {
-
-// });
