65b556c26f480065fda59958b3bcb4d6d06adf9f
thinktwice13 <marioo.krajacic@gmail.com>
2018-04-10 20:18:33 +0200

    Add Context Component Update JWT utils

--- client/src/utils/jwt.js
+++ client/src/utils/jwt.js
@@ -1,5 +1,14 @@
 import jwt from "jsonwebtoken";
 
-export const decodeToken = token =>
-  jwt.decode(token);
+export const decodeToken = tokenName => {
+  const payload = jwt.decode(window.localStorage.getItem(tokenName));
+
+  if (!payload || payload.exp < Date.now() / 1000) {
+    // Remove expired token
+    window.localStorage.removeItem(tokenName);
+    return false;
+  }
+  // Return valid token payload
+  return payload;
+};
 
