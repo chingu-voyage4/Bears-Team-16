09f3de1306d2d7728aa046e74eb2623cf5c76fd0
thinktwice13 <marioo.krajacic@gmail.com>
2018-04-10 20:28:42 +0200

    Use Context in App and Header

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -8,28 +8,52 @@ import Home from "./layout/Home";
 import About from "./layout/About";
 import Browse from "./layout/Browse";
 import Contact from "./layout/Contact";
-/* AUTH COMPONENTS */
-import Login from "./auth/Login";
-import Signup from "./auth/Signup/Signup";
-import AuthExample from "./auth/AuthExample";
-/* OTHER COMPONENTS */
+/* AUTH COMPONENT */
+import Signin from "./auth/Signin";
+/* OTHER */
+import { UserContext } from "./Context";
+import { decodeToken } from "../utils/jwt";
 
-const App = () => (
-  <Router basename={process.env.PUBLIC_URL}>
-      <div >
-      <Header />
-        <div style={{  minHeight: `calc(100vh - 40px)` }}>
-      <Route exact path="/" component={Home} />
-      <Route path="/login" component={Login} />
-      <Route path="/AuthExample" component={AuthExample} />
-      <Route path="/signup" component={Signup} />
-      <Route path="/browse" component={Browse} />
-      <Route path="/about" component={About} />
-      <Route path="/contact" component={Contact} />
-        </div>
-      <Footer />
-    </div>
-  </Router>
-);
+export default class extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: false,
+    };
+  }
 
-export default App;
+  componentWillMount() {
+    this.setState({
+      user: decodeToken(`recipes`),
+    });
+  }
+
+  login = token => {
+    window.localStorage.setItem(`recipes`, token);
+    this.setState({ user: decodeToken(`recipes`) });
+  }
+
+  logout = () => {
+    window.localStorage.removeItem(`recipes`);
+    // TODO invalidate token serverside
+    this.setState({ user: false });
+  }
+
+  render() {
+    return (
+      <UserContext.Provider value={{ user: this.state.user, login: this.login, logout: this.logout }}>
+        <Router basename={process.env.PUBLIC_URL}>
+          <div>
+            <Header />
+            <Route exact path="/" component={Home} />
+            <Route path="/signin" component={Signin} />
+            <Route path="/browse" component={Browse} />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Footer />
+          </div>
+        </Router>
+      </UserContext.Provider>
+    );
+  }
+}
--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -1,31 +1,41 @@
 /* eslint-disable */ // FIXME temporary for deployment test
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { UserContext } from "../../Context"
 import './Header.css';
 import { HeaderStyle, NavBarStyle, NavStyle, Input, NavItem, SignUpButton } from './styles.js';
 
-const Header = () => (
+const Header = () => {
+  return (
     <div style={{ marginBottom: `100px` }}>
-        <HeaderStyle>
-            <div css={{ gridArea: `header-logo` }}>logo</div>
-            <NavBarStyle css={{ gridArea: `header-navbar` }}>
-                <NavStyle>
-                    <Link to="/" style={{ textDecoration: `none` }}><img src='/icons/Home.svg' /><NavItem>HOME</NavItem></Link>
-                </NavStyle>
-                <NavStyle>
-                    <Link to="/browse" style={{ textDecoration: `none` }}><img src='/icons/Browse.svg' /><NavItem>BROWSE</NavItem></Link>
-                </NavStyle>
-                <NavStyle>
-                    <Link to="/about" style={{ textDecoration: `none` }}><img src='/icons/About.svg' /><NavItem>ABOUT</NavItem></Link>
-                </NavStyle>
-                <NavStyle>
-                    <Link to="/contact" style={{ textDecoration: `none` }}><img src='/icons/Contact.svg' /><NavItem>CONTACT</NavItem></Link>
-                </NavStyle>
-                <Input placeholder="Search for recipes.." />
-                <SignUpButton>SIGN IN</SignUpButton>
-            </NavBarStyle>
-        </HeaderStyle>
-    </div>
-);
+    <HeaderStyle>
+      <div css={{ gridArea: `header-logo` }}>logo</div>
+      <NavBarStyle css={{ gridArea: `header-navbar` }}>
+        <NavStyle>
+          <Link to="/" style={{ textDecoration: `none` }}><img src="/icons/Home.svg" /><NavItem>HOME</NavItem></Link>
+        </NavStyle>
+        <NavStyle>
+          <Link to="/browse" style={{ textDecoration: `none` }}><img src="/icons/Browse.svg" /><NavItem>BROWSE</NavItem></Link>
+        </NavStyle>
+        <NavStyle>
+          <Link to="/about" style={{ textDecoration: `none` }}><img src="/icons/About.svg" /><NavItem>ABOUT</NavItem></Link>
+        </NavStyle>
+        <NavStyle>
+          <Link to="/contact" style={{ textDecoration: `none` }}><img src="/icons/Contact.svg" /><NavItem>CONTACT</NavItem></Link>
+        </NavStyle>
+        <Input placeholder="Search for recipes.." />
+        <UserContext.Consumer>
+          {
+            ({user, logout}) => 
+              !!user
+                ? <SignUpButton onClick={logout}>LOGOUT</SignUpButton>
+                : <Link to="/signin" ><SignUpButton>SIGN IN</SignUpButton></Link>
+          }
+        </UserContext.Consumer>
+      </NavBarStyle>
+    </HeaderStyle>
+  </div>
+)
+}
 
 export default Header;
