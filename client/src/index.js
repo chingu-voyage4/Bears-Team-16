9c7500f4c51faeae112ad6a389de0d21bdd21f36
Thomas Lombart <t.lombart97@gmail.com>
2018-04-10 22:30:59 +0200

    Add sass

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import ApolloClient from "apollo-boost";
 import { ApolloProvider } from "react-apollo";
-import './index.css';
+import './main.css';
 import App from './components/App';
 import { baseURL } from "./utils/api";
 import registerServiceWorker from './registerServiceWorker';
