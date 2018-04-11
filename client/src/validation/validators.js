f4ed5644ed2c0db98cb7fd51adaf1a6bf5eced8b
osycon <fredrichawk@gmail.com>
2018-04-11 09:19:17 +0200

    Added test for portion validation and portion validation.

--- client/src/validation/validators.js
+++ client/src/validation/validators.js
@@ -9,3 +9,15 @@ export const titleValidate = value => ({
     ? `Thank you for entering a recipe title!`
     : null,
 });
+
+export const portionsValidate = value => ({
+  error: !value
+    ? `Must contain a portions amount.`
+    : null,
+  warning: value && !/^[0-9]+$/gi.test(value)
+    ? `Field can only contain at most 3 digits.`
+    : null,
+  success: value && /^[0-9]+$/gi.test(value)
+    ? `Thank you for entering a portions amount!`
+    : null,
+});
--- client/src/validation/validators.test.js
+++ client/src/validation/validators.test.js
@@ -18,4 +18,22 @@ describe(`validators`, () => {
       success: `Thank you for entering a recipe title!`,
     });
   });
+
+  it(`should validate amount of portions`, () => {
+    expect(validators.portionsValidate()).toEqual({
+      error: `Must contain a portions amount.`,
+      warning: null,
+      success: null,
+    });
+    expect(validators.portionsValidate(`234dkljsf`)).toEqual({
+      error: null,
+      warning: `Field can only contain at most 3 digits.`,
+      success: null,
+    });
+    expect(validators.portionsValidate(`3`)).toEqual({
+      error: null,
+      warning: null,
+      success: `Thank you for entering a portions amount!`,
+    });
+  });
 });
