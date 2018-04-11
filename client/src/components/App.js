b86cde7955c94045d285a7fb4b3f2dfcef6ad142
osycon <fredrichawk@gmail.com>
2018-04-11 21:48:36 +0200

    Added addrecipe route for now.

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -13,18 +13,20 @@ import Contact from "./layout/Contact";
 import Login from "./auth/Login";
 import Signup from "./auth/Signup/Signup";
 /* OTHER COMPONENTS */
+import AddRecipe from './recipes/AddRecipe';
 
 const App = () => (
-    <Router>
-        <div>
-            <Header />
-            <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
-            <Route path="/browse" component={Browse} />
-            <Route path="/about" component={About} />
-            <Route path="/contact" component={Contact} />
-            <Footer />
+  <Router>
+      <div>
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Route path="/login" component={Login} />
+          <Route path="/signup" component={Signup} />
+          <Route path="/browse" component={Browse} />
+          <Route path="/about" component={About} />
+          <Route path="/contact" component={Contact} />
+          <Route path="/addrecipe" component={AddRecipe} />
+          <Footer />
         </div>
     </Router>
 );
