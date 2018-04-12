2b187626603454e9bed7926b840f43859e1aee25
Ayumi <ayumisaito27@gmail.com>
2018-04-12 21:16:55 +0200

    replace image with transparent svg

--- client/src/components/layout/Footer/Footer.js
+++ client/src/components/layout/Footer/Footer.js
@@ -1,14 +1,19 @@
 import React from 'react';
+import facebook from '../../../assets/facebook.svg'
+import gplus from '../../../assets/gplus.svg'
+import instagram from '../../../assets/instagram.svg'
+import pinterest from '../../../assets/pinterest.svg'
+import twitter from '../../../assets/twitter.svg'
 import './Footer.css';
 
 const Footer = () => (
   <footer>
     <div className="icons">
-      <img className="icon" src="/icons/Facebook.svg" alt="Facebook Icon" />
-      <img className="icon" src="/icons/Gplus.svg" alt="Google+ Icon" />
-      <img className="icon" src="/icons/Instagram.svg" alt="Instagram Icon" />
-      <img className="icon" src="/icons/Pinterest.svg" alt="Pinterest Icon" />
-      <img className="icon" src="/icons/Twitter.svg" alt="Twitter Icon" />
+      <img className="icon" src={facebook} alt="Facebook Icon" />
+      <img className="icon" src={gplus} alt="Google+ Icon" />
+      <img className="icon" src={instagram} alt="instagram Icon" />
+      <img className="icon" src={pinterest} alt="Pinterest Icon" />
+      <img className="icon" src={twitter} alt="Twitter Icon" />
     </div>
     <div className="bottom-wrapper">
       <p className="copyright">© Bear recipes 2018</p>
