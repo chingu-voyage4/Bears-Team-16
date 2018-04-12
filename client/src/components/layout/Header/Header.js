ad2accbe6cc339f37918c0948c19b08ff6349da6
Fred Hawk <osycon@users.noreply.github.com>
2018-04-12 23:15:29 +0200

    Replace icons with SVGs with transparent backgrounds.
    * Replace icons with SVGs with transparent backgrounds.
    * Replace image with transparent svg

--- client/src/components/layout/Header/Header.js
+++ client/src/components/layout/Header/Header.js
@@ -4,6 +4,10 @@ import { Link } from 'react-router-dom';
 import { UserContext } from "../../Context"
 import './Header.css';
 import { HeaderStyle, NavBarStyle, NavStyle, Input, NavItem, SignUpButton } from './styles.js';
+import about from '../../../assets/about.svg'
+import home from '../../../assets/home.svg'
+import browse from '../../../assets/browserecipes.svg'
+import contact from '../../../assets/contact.svg'
 
 const Header = () => {
   return (
@@ -12,21 +16,21 @@ const Header = () => {
       <div css={{ gridArea: `header-logo` }}>logo</div>
       <NavBarStyle css={{ gridArea: `header-navbar` }}>
         <NavStyle>
-          <Link to="/" style={{ textDecoration: `none` }}><img src="/icons/Home.svg" /><NavItem>HOME</NavItem></Link>
+          <Link to="/" style={{ textDecoration: `none` }}><img src={about} /><NavItem>HOME</NavItem></Link>
         </NavStyle>
         <NavStyle>
-          <Link to="/browse" style={{ textDecoration: `none` }}><img src="/icons/Browse.svg" /><NavItem>BROWSE</NavItem></Link>
+          <Link to="/browse" style={{ textDecoration: `none` }}><img src={home} /><NavItem>BROWSE</NavItem></Link>
         </NavStyle>
         <NavStyle>
-          <Link to="/about" style={{ textDecoration: `none` }}><img src="/icons/About.svg" /><NavItem>ABOUT</NavItem></Link>
+          <Link to="/about" style={{ textDecoration: `none` }}><img src={browse} /><NavItem>ABOUT</NavItem></Link>
         </NavStyle>
         <NavStyle>
-          <Link to="/contact" style={{ textDecoration: `none` }}><img src="/icons/Contact.svg" /><NavItem>CONTACT</NavItem></Link>
+          <Link to="/contact" style={{ textDecoration: `none` }}><img src={contact} /><NavItem>CONTACT</NavItem></Link>
         </NavStyle>
         <Input placeholder="Search for recipes.." />
         <UserContext.Consumer>
           {
-            ({user, logout}) => 
+            ({user, logout}) =>
               !!user
                 ? <SignUpButton onClick={logout}>LOGOUT</SignUpButton>
                 : <Link to="/signin" ><SignUpButton>SIGN IN</SignUpButton></Link>
